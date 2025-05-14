1d9a981b692417dd7af00b8be5442f48a1890e5a
Tejashvi Raj <tejashviraj2022@vitbhopal.ac.in>
2025-05-15 00:26:14 +0530

    Create Day01.cpp

---
+++ Day01.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Output message
+    cout << "Hello, World!" << endl;
+    
+    // Asking for user input
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+
+    // Displaying the entered number
+    cout << "You entered: " << number << endl;
+    
+    // Simple conditional check
+    if (number % 2 == 0) {
+        cout << number << " is even." << endl;
+    } else {
+        cout << number << " is odd." << endl;
+    }
+
+    return 0;
+}
