488bb3c4cf5dbe774801d85edcfe3ba68fdac2fd
Tejashvi Raj <tejashviraj2022@vitbhopal.ac.in>
2025-06-01 01:02:21 +0530

    Day17.cpp

--- Day17.cpp
+++ Day17.cpp
@@ -16,19 +16,19 @@ int main() {
     if (marks < 25) {
         cout << "F";
     }
-    else if (marks >= 25 && marks <= 44) {
+    else if (marks <= 44) {
         cout << "E";
     }
-    else if (marks >= 45 && marks <= 49) {
+    else if (marks <= 49) {
         cout << "D";
     }    
-    else if (marks >= 50 && marks <= 59) {
+    else if (marks <= 59) {
         cout << "C";
     }
-    else if (marks >= 60 && marks <= 79) {
+    else if (marks <= 79) {
         cout << "B";
     }    
-    else if (marks >= 80 && marks <= 100) {
+    else if (marks <= 100) {
         cout << "A";
     }    
     return 0;
