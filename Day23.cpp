7cc54099cd8988ae6473a2392ec4af73762750f0
Tejashvi Raj <tejashviraj2022@vitbhopal.ac.in>
2025-06-06 14:39:16 +0530

    Day23.cpp

---
+++ Day23.cpp
@@ -0,0 +1,35 @@
+int main() {
+    for(int i = 0; i < 4; i++) {
+        for(int j = 0; j < 4; j++)  {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+//By intorducing function
+void print1() {
+    for(int i = 0; i < 7; i++) {
+        for(int j = 0; j < 3; j++)  {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+int main() {
+    print1();
+}
+*/
+void print1( int n) {
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++)  {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+int main() {
+    int n;
+    cin >> n;
+    print1(n);
+}
