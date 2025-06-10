25d48c3f4a358669511d14b6054c1c4c1f6600a0
Tejashvi Raj <tejashviraj2022@vitbhopal.ac.in>
2025-06-11 02:11:09 +0530

    Day27.cpp

--- Day27.cpp
+++ Day27.cpp
@@ -1,28 +1,156 @@
 #include <iostream>
 using namespace std;
 
-void countFrequency(int arr[], int size) {
-    bool visited[100] = {false};
-
-    for (int i = 0; i < size; i++) {
-        if (visited[i]) continue;
-
-        int count = 1;
-        for (int j = i + 1; j < size; j++) {
-            if (arr[i] == arr[j]) {
-                visited[j] = true;
-                count++;
-            }
+void Print5(int n) {
+    for (int i = 1; i<= n; i++) {
+        for( int j = 0; j<= n-i+1; j++) {
+            cout << "*" ;
         }
-        cout << arr[i] << " appears " << count << " times." << endl;
+        cout<< endl;
     }
 }
 
 int main() {
-    int arr[] = {1, 2, 2, 3, 4, 1, 5, 3};
-    int size = sizeof(arr) / sizeof(arr[0]);
+    int t;
+    cin>> t;
+    for(int i = 0; i<t; i++) {
+        int n;
+        cin >> n;
+        Print5(n);
+    }
+}
 
-    countFrequency(arr, size);
+void Print6(int n) {
+    for (int i = 1; i<= n; i++) {
+        for( int j = 1; j<= n-i+1; j++) {
+            cout << j << " " ;
+        }
+        cout<< endl;
+    }
+}
 
-    return 0;
+int main() {
+    int t;
+    cin>> t;
+    for(int i = 0; i<t; i++) {
+        int n;
+        cin >> n;
+        Print6(n);
+    }
+}
+
+void Print7(int n) {
+    for (int i = 0; i<= n; i++) {
+        //space
+        for( int j = 0; j<= n-i-1; j++) {
+            cout <<  " " ;
+        }
+
+        //star
+        for( int j = 0; j< 2*i+1; j++) {
+            cout <<  "*" ;
+        }    
+
+        //space
+        for( int j = 0; j<= n-i-1; j++) {
+            cout <<  " " ;
+        }
+        cout<< endl;
+    }
+}
+int main() {
+    int t;
+    cin>> t;
+    for(int i = 0; i<t; i++) {
+        int n;
+        cin >> n;
+        Print7(n);
+    }
+}    
+//reverse pattern
+
+void Print8(int n) {
+    for (int i = 0; i<= n; i++) {
+        //space
+        for( int j = 0; j< i; j++) {
+            cout <<  " " ;
+        }
+
+        //star
+        for( int j = 0; j< 2*n -(2*i+1); j++) {
+            cout <<  "*" ;
+        }    
+
+        //space
+        for( int j = 0; j< i; j++) {
+            cout <<  " " ;
+        }
+        cout<< endl;
+    }
+}
+
+    
+int main() {
+    int t;
+    cin>> t;
+    for(int i = 0; i<t; i++) {
+        int n;
+        cin >> n;
+        Print8(n);
+    }
+}
+*/
+
+//both at a time
+
+void Print7(int n) {
+    for (int i = 0; i<= n; i++) {
+        //space
+        for( int j = 0; j<= n-i-1; j++) {
+            cout <<  " " ;
+        }
+
+        //star
+        for( int j = 0; j< 2*i+1; j++) {
+            cout <<  "*" ;
+        }    
+
+        //space
+        for( int j = 0; j<= n-i-1; j++) {
+            cout <<  " " ;
+        }
+        cout<< endl;
+    }
+}
+
+void Print8(int n) {
+    for (int i = 0; i<= n; i++) {
+        //space
+        for( int j = 0; j< i; j++) {
+            cout <<  " " ;
+        }
+
+        //star
+        for( int j = 0; j< 2*n -(2*i + 1); j++) {
+            cout <<  "*" ;
+        }    
+
+        //space
+        for( int j = 0; j< i; j++) {
+            cout <<  " " ;
+        }
+        cout<< endl;
+    }
+}
+
+    
+int main() {
+    int t;
+    cin>> t;
+    for(int i = 0; i<t; i++) {
+        int n;
+        cin >> n;
+        Print7(n);
+        Print8(n);
+    }
 }
