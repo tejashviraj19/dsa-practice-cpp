c2465ae9e79f12f269e0faed2bdd67ab491eb573
Tejashvi Raj <tejashviraj2022@vitbhopal.ac.in>
2025-07-11 00:16:08 +0530

    Day50.cpp

---
+++ Day50.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int lcm(int a, int b) {
+    return (a * b) / gcd(a, b);
+}
+
+int main() {
+    int a = 12, b = 18;
+    cout << "LCM: " << lcm(a, b) << endl;
+    return 0;
+}
