d8db15eabb023f4d0d7247d419526da8f0b8adea
Tejashvi Raj <tejashviraj2022@vitbhopal.ac.in>
2025-12-09 20:54:42 +0530

    Day185.cpp

---
+++ Day185.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    vector<int> findDuplicates(vector<int>& arr) {
+        int n=arr.size();
+        unordered_map<int,int>mp;
+        for(auto it:arr)
+        {
+            mp[it]++;
+        }
+        int o=-1;
+        vector<int>ans;
+        for(auto it:mp)
+        {
+            if(it.second>1)
+            {
+                o=it.first;
+                ans.push_back(o);
+            }
+        }
+        return ans;
+    }
+};
