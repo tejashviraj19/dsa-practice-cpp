901c3ffced4b5ca0f16b04b838d716545e7c2b8e
Tejashvi Raj <tejashviraj2022@vitbhopal.ac.in>
2025-12-22 23:45:45 +0530

    Day198.cpp

---
+++ Day198.cpp
@@ -0,0 +1,19 @@
+// User function template for C++
+class Solution {
+  public:
+    int rowWithMax1s(vector<vector<int>> &arr) {
+        // code here
+        int ans = 0 , maxi = 0 ;
+        int n = arr.size() ,m= arr[0].size();
+        for(int i = 0 ; i < n ; i++ ){
+            int sum = 0 ;
+            for(int j = 0 ; j < m ; j++){
+                sum += arr[i][j];
+            }
+            if(maxi < sum ) ans = i;
+            maxi = max(maxi , sum);
+        }
+        
+        return maxi != 0 ? ans : -1;
+    }
+};
